674351d37e3bc379cc3808b8ef01df6a22b4b43f
tindlejordantindlejordan-star <tindlejordantindlejordan@gmail.com>
2026-08-24 10:08:04 -0700

    Update dashboard.js

--- dist/dashboard.js
+++ dist/dashboard.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://supabase.com/dashboard/project/mbxqkkkynkddrecpgyrf/settings/api-keys";
+const SUPABASE_URL = "https://supabase.com/dashboard/project/mbxqkkkynkddrecpgyrf";
 
 const SUPABASE_KEY = "sb_publishable_g26n0VVcxkp0Yo8vgZbrOg_ytLd42rK";
 
