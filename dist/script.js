2e696ae49d6f96c6c7c9d7ec42034e63acdc88c4
tindlejordantindlejordan-star <tindlejordantindlejordan@gmail.com>
2026-08-23 22:23:04 -0700

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -5,6 +5,13 @@ const SUPABASE_KEY = "YOUR_ANON_KEY";
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
+);const SUPABASE_URL = "YOUR_SUPABASE_URL";
+
+const SUPABASE_KEY = "YOUR_ANON_KEY";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
 async function signup(){
 
 const email =
@@ -447,5 +454,68 @@ doc.text(
 doc.save(
 "Rental-Analysis-Report.pdf"
 );
+
+}
+async function signup(){
+
+    const email =
+    document.getElementById("email").value;
+
+    const password =
+    document.getElementById("password").value;
+
+
+    const {error} =
+    await supabaseClient.auth.signUp({
+        email,
+        password
+    });
+
+
+    if(error){
+        alert(error.message);
+    }
+    else{
+        alert("Account created!");
+    }
+
+}
+
+
+
+async function login(){
+
+    const email =
+    document.getElementById("email").value;
+
+    const password =
+    document.getElementById("password").value;
+
+
+    const {error} =
+    await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+
+    if(error){
+        alert(error.message);
+    }
+    else{
+        alert("Logged in!");
+
+        window.location.href="dashboard.html";
+    }
+
+}
+
+
+
+async function logout(){
+
+    await supabaseClient.auth.signOut();
+
+    window.location.href="index.html";
 
 }
